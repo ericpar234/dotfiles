cd1f28de765deb3bb94e59a8404dd8e88e2e298c
ericpar234 <ericpar234@gmail.com>
2024-03-08 17:26:08 -0500

    submodukes

--- configure.sh
+++ configure.sh
@@ -4,6 +4,11 @@
 git submodule init
 git submodule update
 
+#curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+#chmod u+x nvim.appimage
+#./nvim.appimage
+#rm ./nvim.appimage
+
 # Install starship prompt
 curl -sS https://starship.rs/install.sh | sh
 
