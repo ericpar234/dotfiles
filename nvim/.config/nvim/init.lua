85fa392e84ff51461a0c49f8716c47d75cefde76
ericpar234 <ericpar234@gmail.com>
2024-03-16 21:14:07 -0400

    update mappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,12 @@
+--- Define vim for linter
+vim = vim or {}
+
 -- Bootstrap packer.nvim
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  local result = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  local result = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
   print("Git clone result: " .. result)
   vim.cmd [[packadd packer.nvim]]
   print("packadd command executed")
@@ -17,9 +20,9 @@ vim.g.maplocalleader = ' '
 local o = vim.o
 
 o.expandtab = true
-o.smartindent = true 
+o.smartindent = true
 o.tabstop = 2
-o.shiftwidth = 2 
+o.shiftwidth = 2
 
 -- Enable Term gui colors
 vim.opt.termguicolors = true
@@ -27,66 +30,66 @@ vim.opt.termguicolors = true
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- Packer manages itself
   -- Add your plugins here
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'hrsh7th/cmp-buffer' -- Buffer completions
-  use 'hrsh7th/cmp-path' -- Path completions
-  use 'L3MON4D3/LuaSnip' -- Snippet engine
+  use 'hrsh7th/cmp-nvim-lsp'   -- LSP source for nvim-cmp
+  use 'hrsh7th/cmp-buffer'     -- Buffer completions
+  use 'hrsh7th/cmp-path'       -- Path completions
+  use 'L3MON4D3/LuaSnip'       -- Snippet engine
 
   use {
-       "williamboman/nvim-lsp-installer",
-       "neovim/nvim-lspconfig",
-  }  
+    "williamboman/nvim-lsp-installer",
+    "neovim/nvim-lspconfig",
+  }
 
   use {
     'hrsh7th/nvim-cmp',
     requires = {
-      'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
-      'hrsh7th/cmp-buffer', -- Buffer completions
-      'hrsh7th/cmp-path', -- Path completions
-      'hrsh7th/cmp-cmdline', -- Cmdline completions
+      'hrsh7th/cmp-nvim-lsp',     -- LSP source for nvim-cmp
+      'hrsh7th/cmp-buffer',       -- Buffer completions
+      'hrsh7th/cmp-path',         -- Path completions
+      'hrsh7th/cmp-cmdline',      -- Cmdline completions
       'saadparwaiz1/cmp_luasnip', -- Snippet completions
-      'L3MON4D3/LuaSnip', -- Snippet engine
+      'L3MON4D3/LuaSnip',         -- Snippet engine
     }
   }
   use {
-  'nvim-treesitter/nvim-treesitter',
-        run = function()
-        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
   }
   use {
     'kyazdani42/nvim-tree.lua',
     requires = {
       'kyazdani42/nvim-web-devicons', -- optional, for file icons
     },
-    config = function() require'nvim-tree'.setup {} end
+    config = function() require 'nvim-tree'.setup {} end
   }
 
   use 'tpope/vim-fugitive'
-  
+
   use {
-	  'lewis6991/gitsigns.nvim',
-	  requires = {
-		  'nvim-lua/plenary.nvim'
-	  },
-	  config = function()
-		  require('gitsigns').setup()
-	  end
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('gitsigns').setup()
+    end
   }
 
   use 'nvim-lualine/lualine.nvim' -- Statusline
 
-  use 'folke/which-key.nvim' -- Plugin for keybinding hints
-  use {'romgrk/barbar.nvim',
-    requires = {'kyazdani42/nvim-web-devicons'}
+  use 'folke/which-key.nvim'      -- Plugin for keybinding hints
+  use { 'romgrk/barbar.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' }
   }
 
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
-  
+
   use { "gmr458/vscode_modern_theme.nvim" }
   use { "catppuccin/nvim", as = "catppuccin" }
 
@@ -117,7 +120,7 @@ require('packer').startup(function(use)
         theme = 'hyper',
         config = {
           week_header = {
-           enable = true,
+            enable = true,
           },
           shortcut = {
             { desc = '󰊳 Update', group = '@property', action = 'PackerSync', key = 'u' },
@@ -129,11 +132,11 @@ require('packer').startup(function(use)
               action = 'Telescope find_files',
               key = 'f',
             },
-           {
-	      desc = ' Configuration',
-	      group = '@property',
+            {
+              desc = ' Configuration',
+              group = '@property',
               action = 'e ~/.config/nvim/init.lua',
-	      key = 'c',
+              key = 'c',
             },
             {
               desc = ' dotfiles',
@@ -142,17 +145,15 @@ require('packer').startup(function(use)
               key = 'd',
             },
           },
-	}
-       }
+        }
+      }
     end,
-    requires = {'nvim-tree/nvim-web-devicons'}
+    requires = { 'nvim-tree/nvim-web-devicons' }
   }
-
-
 end)
 
 require("catppuccin").setup({
-	flavor = "mocha",
+  flavor = "mocha",
   integrations = {
     treesitter = true,
     which_key = true,
@@ -162,18 +163,18 @@ require("catppuccin").setup({
 )
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "*",
-    callback = function()
-        package.loaded["feline"] = nil
-        package.loaded["catppuccin.groups.integrations.feline"] = nil
-        require("feline").setup {
-            components = require("catppuccin.groups.integrations.feline").get(),
-        }
-    end,
+  pattern = "*",
+  callback = function()
+    package.loaded["feline"] = nil
+    package.loaded["catppuccin.groups.integrations.feline"] = nil
+    require("feline").setup {
+      components = require("catppuccin.groups.integrations.feline").get(),
+    }
+  end,
 })
 
 require('nvim-web-devicons').setup {
- -- your configuration here
+  -- your configuration here
 }
 vim.cmd.colorscheme "catppuccin"
 
@@ -213,7 +214,7 @@ local mappings = {
   ["<leader>o"] = { "<cmd>e .<cr>", "Open File Explorer" },
   ["<leader>t"] = { "<cmd>split | terminal<cr>", "Open Terminal" },
   --- Close window
-  ["<leader>c"] = { "<C-w>c", "Close Window" }, 
+  ["<leader>c"] = { "<C-w>c", "Close Window" },
 }
 wk.register(git_mappings, { prefix = "<leader>" })
 wk.register(mappings, { prefix = "<leader>" })
@@ -224,23 +225,23 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Ctrl+h/j/k/l to navigate windows
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 
 -- Buffer navigation
-map('n', '<Leader>bn', ':bnext<CR>', opts) -- Next buffer
+map('n', '<Leader>bn', ':bnext<CR>', opts)     -- Next buffer
 map('n', '<Leader>bp', ':bprevious<CR>', opts) -- Previous buffer
-map('n', '<Leader>bd', ':bd<CR>', opts) -- Close buffer
+map('n', '<Leader>bd', ':bd<CR>', opts)        -- Close buffer
 
 -- Example mapping for opening the terminal
 map('n', '<Leader>t', ':split | terminal<CR>', opts) -- Open terminal in horizontal split
-vim.api.nvim_set_keymap('n', '<Leader>f', ':Telescope find_files<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>f', ':Telescope find_files<CR>', { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
-local cmp = require'cmp'
+local cmp = require 'cmp'
 
 cmp.setup({
   snippet = {
@@ -264,36 +265,36 @@ cmp.setup({
 
 
 require("nvim-lsp-installer").setup({
-    automatic_installation = true,
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
+  automatic_installation = true,
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
     }
+  }
 })
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
 
-local lspconfig = require'lspconfig'
-lspconfig.jedi_language_server.setup{}
-lspconfig.bashls.setup{}
-lspconfig.clangd.setup{}
-lspconfig.cmake.setup{}
-lspconfig.dockerls.setup{}
-lspconfig.jsonls.setup{}
-lspconfig.html.setup{}
-lspconfig.zk.setup{}
-lspconfig.lua_ls.setup{}
+local lspconfig = require 'lspconfig'
+lspconfig.jedi_language_server.setup {}
+lspconfig.bashls.setup {}
+lspconfig.clangd.setup {}
+lspconfig.cmake.setup {}
+lspconfig.dockerls.setup {}
+lspconfig.jsonls.setup {}
+lspconfig.html.setup {}
+lspconfig.zk.setup {}
+lspconfig.lua_ls.setup {}
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open diagnostic float' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic' })
+vim.keymap.set('n', '<leader>Q', vim.diagnostic.setloclist, { desc = 'Set location list' })
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -305,8 +306,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf, desc = 'Go to declaration' })
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
@@ -320,40 +320,39 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-    vim.lsp.buf.format { async = true }
+    vim.keymap.set('n', '<space>F', function()
+      vim.lsp.buf.format { async = true }
     end, opts)
   end,
 })
 
 --- Lsp which-key
 
-lsp_mappings = {
-  gD = "Go to definition", 
-  gd = "Go to declaration",
-  K = "Hover",
-  gi = "Go to implementation",
-  ["<C-g>"] = "Signature help",
-  ["<space>wa"] = "Add workspace folder",
-  ["<space>wr"] = "Remove workspace folder",
-  ["<space>wl"] = "List workspace folders",
-  ["<space>D"] = "Type definition",
-  ["<space>rn"] = "Rename",
-  ["<space>ca"] = "Code action",
-  gr = "References",
-  ["<space>f"] = "Format",
-}
-
-wk.register(lsp_mappings, { prefix = "<leader>" })
+--- lsp_mappings = {
+---   gd = "Go to declaration",
+---   K = "Hover",
+---   gi = "Go to implementation",
+---   ["<C-g>"] = "Signature help",
+---   ["<space>wa"] = "Add workspace folder",
+---   ["<space>wr"] = "Remove workspace folder",
+---   ["<space>wl"] = "List workspace folders",
+---   ["<space>D"] = "Type definition",
+---   ["<space>rn"] = "Rename",
+---   ["<space>ca"] = "Code action",
+---   gr = "References",
+---   ["<space>F"] = "Format",
+--- }
+--- 
+--- wk.register(lsp_mappings, { prefix = "<leader>" })
 
 
 require('gitsigns').setup {
   signs = {
-    add = {hl = 'GitSignsAdd', text = '│', numhl='GitSignsAddNr', linehl='GitSignsAddLn'},
-    change = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+    change = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    delete = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    topdelete = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
   },
   numhl = false,
   linehl = false,
@@ -397,13 +396,13 @@ require('gitsigns').setup {
       if vim.wo.diff then return ']c' end
       vim.schedule(function() gs.next_hunk() end)
       return '<Ignore>'
-    end, {expr=true})
+    end, { expr = true })
 
     map('n', '[c', function()
       if vim.wo.diff then return '[c' end
       vim.schedule(function() gs.prev_hunk() end)
       return '<Ignore>'
-    end, {expr=true})
+    end, { expr = true })
 
     --- Actions
     --- map('n', '<leader>hs', gs.stage_hunk)
@@ -421,47 +420,45 @@ require('gitsigns').setup {
     --- map('n', '<leader>td', gs.toggle_deleted)
 
     ---  Text object
-   --- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    --- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end
 }
 
 
-    gitsigns_mapping = {
-        ["h"] = {
-		name="Gitsigns",
-		s = { "<cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage Hunk" },
-		r = { "<cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset Hunk" },
-		S = { "<cmd>lua require('gitsigns').stage_buffer()<CR>", "Stage Buffer" },
-		u = { "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Undo Stage Hunk" },
-		R = { "<cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset Buffer" },
-		p = { "<cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk" },
-		b = { "<cmd>lua require('gitsigns').blame_line(true)<CR>", "Blame" },
-		d = { "<cmd>lua require('gitsigns').diffthis()<CR>", "Diff" },
-		D = { "<cmd>lua require('gitsigns').diffthis('HEAD')<CR>", "Diff HEAD" },
-		d = { "<cmd>lua require('gitsigns').toggle_deleted()<CR>", "Toggle Deleted" },
-	        ["t"] = {
-		        name="Toggle",
-	        	b = { "<cmd>lua require('gitsigns').toggle_current_line_blame()<CR>", "Toggle Blame" },
-		        d = { "<cmd>lua require('gitsigns').toggle_deleted()<CR>", "Toggle Deleted" },
-                }
-	},
+gitsigns_mapping = {
+  ["h"] = {
+    name = "Gitsigns",
+    s = { "<cmd>lua require('gitsigns').stage_hunk()<CR>", "Stage Hunk" },
+    r = { "<cmd>lua require('gitsigns').reset_hunk()<CR>", "Reset Hunk" },
+    S = { "<cmd>lua require('gitsigns').stage_buffer()<CR>", "Stage Buffer" },
+    u = { "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>", "Undo Stage Hunk" },
+    R = { "<cmd>lua require('gitsigns').reset_buffer()<CR>", "Reset Buffer" },
+    p = { "<cmd>lua require('gitsigns').preview_hunk()<CR>", "Preview Hunk" },
+    b = { "<cmd>lua require('gitsigns').blame_line(true)<CR>", "Blame" },
+    d = { "<cmd>lua require('gitsigns').diffthis()<CR>", "Diff" },
+    D = { "<cmd>lua require('gitsigns').diffthis('HEAD')<CR>", "Diff HEAD" },
+    d = { "<cmd>lua require('gitsigns').toggle_deleted()<CR>", "Toggle Deleted" },
+    ["t"] = {
+      name = "Toggle",
+      b = { "<cmd>lua require('gitsigns').toggle_current_line_blame()<CR>", "Toggle Blame" },
+      d = { "<cmd>lua require('gitsigns').toggle_deleted()<CR>", "Toggle Deleted" },
     }
+  },
+}
 
-    gitsigns_visual_mapping = {
-	["h"] = {
-		name="Gitsigns",
-		s = { "<cmd>lua require('gitsigns').stage_hunk({vim.fn.line('.'), vim.fn.line('v')})<CR>", "Stage Hunk" },
-		r = { "<cmd>lua require('gitsigns').reset_hunk({vim.fn.line('.'), vim.fn.line('v')})<CR>", "Reset Hunk" },
-	}
-    }
+gitsigns_visual_mapping = {
+  ["h"] = {
+    name = "Gitsigns",
+    s = { "<cmd>lua require('gitsigns').stage_hunk({vim.fn.line('.'), vim.fn.line('v')})<CR>", "Stage Hunk" },
+    r = { "<cmd>lua require('gitsigns').reset_hunk({vim.fn.line('.'), vim.fn.line('v')})<CR>", "Reset Hunk" },
+  }
+}
 
-    wk.register(gitsigns_mapping, { prefix = "<leader>" })
-    wk.register(gitsigns_visual_mapping, { mode = "v", prefix = "<leader>" })
+wk.register(gitsigns_mapping, { prefix = "<leader>" })
+wk.register(gitsigns_visual_mapping, { mode = "v", prefix = "<leader>" })
 
 --- Tab buffer switching
 -- Switch to the next buffer
 vim.api.nvim_set_keymap('n', '<Tab>', ':bnext<CR>', { noremap = true, silent = true })
 -- Switch to the previous buffer
 vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprevious<CR>', { noremap = true, silent = true })
-
-
