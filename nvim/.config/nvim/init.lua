678d0840cf53d942b312498b65ce271958425448
ericpar234 <ericpar234@gmail.com>
2024-04-01 11:19:30 -0400

    Copilot third party

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -140,7 +140,11 @@ require('packer').startup(function(use)
   -- Icons in suggestions
   use 'onsails/lspkind.nvim'
 
+  -- init.lua dev
   use 'folke/neodev.nvim'
+
+  -- copilot
+  use 'zbirenbaum/copilot.lua'
 
   -- Dashboard
   use {
@@ -234,7 +238,52 @@ require('telescope').setup {
     },
   },
 }
+
 require 'telescope'.load_extension('project')
+
+-- Copilot
+require('copilot').setup({
+  panel = {
+    enabled = true,
+    auto_refresh = true,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+    layout = {
+      position = "bottom", -- | top | left | right
+      ratio = 0.4
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      --accept = "<M-l>",
+      accept = "<Tab>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-j>",
+      prev = "<M-k>",
+      --dismiss = "<C-]>",
+    },
+  },
+  filetypes = {
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = 'node', -- Node.js version must be > 18.x
+  server_opts_overrides = {},
+})
 
 vim.cmd.colorscheme "catppuccin"
 
